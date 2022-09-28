9c5e61ea92021636d55dc798d5f2c9303a8ed644
kkashTLG <kevin.kash@tlgcohort.com>
2022-09-28 20:46:40 +0000

    mixed lists

---
+++ lab14.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+def main():
+        userName = input("Please enter your name: ")
+        dayOfWeek = input("Please enter the day of the week: ")
+        print("Hello, " + userName + "! Happy " + dayOfWeek + "!")
+        
+
+main()
---
+++ mix-list/mixlist01.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+# creeate a list with 3 values
+my_list = [ "192.168.0.5", 5060 , "UP" ]
+
+
+# display value of first item in my_list
+print("The first item in the list (IP): " + my_list[0] )
+
+# display value of the second iem in my_list
+print("The second item in the list (port): " + str(my_list[1]) )
+
+# display value of the third item in my_list
+print("The last item in the list (state): " + my_list[2] )
+
