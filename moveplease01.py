f8847fe10a14be02db5262d14d898e9ce9743d9a
kkashTLG <kevin.kash@tlgcohort.com>
2022-09-29 19:56:10 +0000

    renaming and moving files

---
+++ moveplease01.py
@@ -0,0 +1,21 @@
+#!/user/bin/env python3
+"""A simple script to move two files into ceph_storage/
+    Alta3 Research | rsfeeser@alta3.com"""
+
+# standard library imports
+import shutil
+import os
+
+def main():
+    """called at runtime"""
+    os.chdir('/home/student/mycode/') #move into this working directory
+    shutil.move('raynor.obj', 'ceph_storage/') #try moving the file raynor.obj into ceph_storage/ dir
+
+    # program will pause while we accept input
+    xname = input('What is the new name for kerrigan.obj? ') # collect stringg input from the user
+    shutil.move('ceph_storage/kerrigan.obj', 'ceph_storage/' + xname) # moving kerrigan.obj into ceph_storage/ with new name
+
+main() # this calls our main function
+
+
+
